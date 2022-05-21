d54caf4736927d462e2a43e93743622a71576aa7
Ssarkos <81178115+Ssarkos@users.noreply.github.com>
2022-05-21 17:55:47 +0200

    Add files via upload

---
+++ Auto Weapon Detection/Auto Weapon Detection/Menu.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Auto_Weapon_Detection
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.lblLastWeapon = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Detector = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(138, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(25, 30);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "x";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMinimize.ForeColor = System.Drawing.Color.White;
+            this.btnMinimize.Location = new System.Drawing.Point(113, -1);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(25, 30);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // lblLastWeapon
+            // 
+            this.lblLastWeapon.AutoSize = true;
+            this.lblLastWeapon.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLastWeapon.ForeColor = System.Drawing.Color.White;
+            this.lblLastWeapon.Location = new System.Drawing.Point(23, 49);
+            this.lblLastWeapon.Name = "lblLastWeapon";
+            this.lblLastWeapon.Size = new System.Drawing.Size(40, 17);
+            this.lblLastWeapon.TabIndex = 1;
+            this.lblLastWeapon.Text = "None";
+            this.lblLastWeapon.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDrag);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(21, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Weapon";
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDrag);
+            // 
+            // Detector
+            // 
+            this.Detector.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Detector_DoWork);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(164, 85);
+            this.Controls.Add(this.lblLastWeapon);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDrag);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnExit;
+        private Button btnMinimize;
+        private Label label4;
+        private Label lblLastWeapon;
+        private System.ComponentModel.BackgroundWorker Detector;
+    }
+}
---
+++ Auto Weapon Detection/Auto Weapon Detection/Menu.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace Auto_Weapon_Detection
+{
+    public partial class Menu : Form
+    {
+        
+        public Menu()
+        {
+            
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 15, 15));
+            
+            Detector.RunWorkerAsync();           
+        }
+
+        
+        private void Detector_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            Graphics g;
+            Bitmap bmp;
+
+            bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width / 8, Screen.PrimaryScreen.Bounds.Height / 4);
+            g = Graphics.FromImage(bmp);
+
+            Image<Gray, Byte>[] guns = new Image<Gray, Byte>[] {
+                    new Image<Gray, Byte>(@"./img/Melee.png"),
+                    new Image<Gray, Byte>(@"./img/Classic.png"),
+                    new Image<Gray, Byte>(@"./img/Shorty.png"),
+                    new Image<Gray, Byte>(@"./img/Frenzy.png"),
+                    new Image<Gray, Byte>(@"./img/Ghost.png"),
+                    new Image<Gray, Byte>(@"./img/Sheriff.png"),
+                    new Image<Gray, Byte>(@"./img/Stinger.png"),
+                    new Image<Gray, Byte>(@"./img/Spectre.png"),
+                    new Image<Gray, Byte>(@"./img/Bucky.png"),
+                    new Image<Gray, Byte>(@"./img/Judge.png"),
+                    new Image<Gray, Byte>(@"./img/Bulldog.png"),
+                    new Image<Gray, Byte>(@"./img/Guardian.png"),
+                    new Image<Gray, Byte>(@"./img/Phantom.png"),
+                    new Image<Gray, Byte>(@"./img/Vandal.png"),
+                    new Image<Gray, Byte>(@"./img/Marshal.png"),
+                    new Image<Gray, Byte>(@"./img/Operator.png"),
+                    new Image<Gray, Byte>(@"./img/Ares.png"),
+                    new Image<Gray, Byte>(@"./img/Odin.png")
+                };
+
+            string[] gunNames = new string[]
+            {
+                 "Melee", "Classic", "Shorty", "Frenzy", "Ghost", "Sheriff", "Stinger", "Spectre", "Bucky", "Judge", "Bulldog", "Guardian", "Phantom", "Vandal", "Marshal", "Operator", "Ares", "Odin"
+            };
+
+            List<double> confidences = new List<double>();
+
+
+            while (true)
+            {
+
+                confidences.Clear();
+                this.TopMost = true;
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.Width / 6 * 5, Screen.PrimaryScreen.Bounds.Height / 24 * 15, 0, 0, new Size(bmp.Width, bmp.Height));              
+
+                Image<Gray, Byte> sourceImage = bmp.ToImage<Gray, Byte>();
+                sourceImage = sourceImage.ThresholdBinary(new Gray(160), new Gray(255));
+
+                List<double> scores = new List<double>();
+
+                for (int i = 0; i < guns.Length; i++)
+                {
+                    
+                    Image<Gray, float> resultImage = sourceImage.MatchTemplate(guns[i], TemplateMatchingType.CcoeffNormed);
+                    float[,,] matches = resultImage.Data;
+                    for (int y = 0; y < matches.GetLength(0); y++)
+                    {
+                        for (int x = 0; x < matches.GetLength(1); x++)
+                        {
+                            double matchScore = matches[y, x, 0];
+                            scores.Add(matchScore);
+                        }
+                    }
+
+                    confidences.Add(scores.Max());
+                    scores.Clear();
+                }
+
+                double max = confidences.Max();
+
+                if (max > 0.8)
+                    lblLastWeapon.Text = gunNames[confidences.IndexOf(max)];
+                
+                Thread.Sleep(1);
+            }
+        }
+
+
+
+        //
+        // UI Stuff
+        //
+
+        [DllImport("user32.dll")]
+        public static extern bool AllowSetForegroundWindow(uint dwProcessId);
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        private void mouseDrag(object sender, MouseEventArgs e)
+        {
+           
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
